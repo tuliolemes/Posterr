7684ed46829c89cd9ae75517c2ae48daf55948c1
tuliolemes <tuliohseixas@gmail.com>
2022-05-20 15:17:28 -0300

    creating profile signals

--- profiles/models.py
+++ profiles/models.py
@@ -6,17 +6,17 @@ from django.core.validators import RegexValidator
 class Profile(models.Model):
     alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
 
-    #user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=14, validators=[alphanumeric])
-    following = models.ManyToManyField(User, related_name='following', black=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #username = models.CharField(max_length=14, validators=[alphanumeric])
+    following = models.ManyToManyField(User, related_name='following', blank=True)
     #NumberOfFollowers = models.BigIntegerField()
     joined = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.username
+        return self.user.username
 
     def profiles_posts(self):
-        pass
+        return self.post_set.all()
 
     class Meta:
         ordering = ['-joined']
